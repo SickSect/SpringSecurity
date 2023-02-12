42196e5e0113fb10a73deaf219562154c64f8ed6
sicksect <sleepyboy906sb@gmail.com>
2023-02-12 18:18:50 +0300

    Progress with security

--- bootExmpl/src/main/java/com/boot/ugina/bootexmpl/config/JwtAuthFilter.java
+++ bootExmpl/src/main/java/com/boot/ugina/bootexmpl/config/JwtAuthFilter.java
@@ -4,19 +4,48 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JwtAuthFilter extends OncePerRequestFilter {
-    @Autowired
-    private JWTGenerator tokenGenerator;
-    @Autowired
-    private CustomerUserDetailService customerUserDetailService;
 
+    private JWTGenerator tokenGenerator;
+    private CustomerUserDetailService customerUserDetailService;
+     private Logger log = LoggerFactory.getLogger(JwtAuthFilter.class);
+    @Autowired
+    public JwtAuthFilter(JWTGenerator gen, CustomerUserDetailService ser){
+        this.tokenGenerator = gen;
+        this.customerUserDetailService = ser;
+    }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getJwtFromRequest(request);
+        if (StringUtils.hasText(token) && tokenGenerator.validateToken(token)){
+            String username = tokenGenerator.getUsernameFromJwt(token);
+            UserDetails details = customerUserDetailService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    UsernamePasswordAuthenticationToken.authenticated(details, null, details.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            filterChain.doFilter(request, response);
+        }
+    }
 
+    private String getJwtFromRequest(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            log.info("Get bearer token");
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
     }
 }
--- bootExmpl/src/main/java/com/boot/ugina/bootexmpl/config/SecurityConfig.java
+++ bootExmpl/src/main/java/com/boot/ugina/bootexmpl/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -48,7 +49,7 @@ public class SecurityConfig {
                 .authorizeRequests().requestMatchers("/api/auth/*").permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic();
-
+        http.addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -61,5 +62,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(){
+        return new JwtAuthFilter();
     }
 }
