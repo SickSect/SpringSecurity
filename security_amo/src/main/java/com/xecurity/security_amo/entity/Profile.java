baa4a9588339616c531db8d5b1bca689f34047c7
sicksect <sleepyboy906sb@gmail.com>
2023-02-13 23:50:24 +0300

    add

--- security_amo/src/main/java/com/xecurity/security_amo/entity/Profile.java
+++ security_amo/src/main/java/com/xecurity/security_amo/entity/Profile.java
@@ -1,26 +1,63 @@
 package com.xecurity.security_amo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data // don't forget to write them by your self
 @NoArgsConstructor
 @AllArgsConstructor
-public class Profile {
+public class Profile implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String surname;
     private Integer age;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING) //we add this annotation bc Role is enum
+    private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+/*
+        this method
+        return list of roles
+*/
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- security_amo/src/main/java/com/xecurity/security_amo/repo/ProfileRepo.java
+++ security_amo/src/main/java/com/xecurity/security_amo/repo/ProfileRepo.java
@@ -2,6 +2,11 @@ package com.xecurity.security_amo.repo;
 
 import com.xecurity.security_amo.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface ProfileRepo extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByEmail(String email);
 }
